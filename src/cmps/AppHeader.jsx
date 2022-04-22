294a0b9e2db568103a05d3b4aa1abfb89955c006
Angelica Serebnitski <angelsa9292@gmail.com>
2022-04-22 14:20:36 +0300

    use history

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -1,13 +1,16 @@
 import { NavLink } from 'react-router-dom'
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min'
+
 
 export function AppHeader() {
+   const history =useHistory()
 
 
     return (
         <header className="app-header" >
-            <div className='logo' onClick={() => this.props.history.push('/')}>
-                <img src="./img/logo.png" alt="logo" />
-            </div>
+                <div className='logo' onClick={() => history.push('/')}>
+                    <img src="./img/logo.png" alt="logo" />
+                </div>
 
             <nav className="main-nav">
                 <NavLink activeClassName="my-active" exact to="/">Home</NavLink>
