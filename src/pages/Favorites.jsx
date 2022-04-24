3ea81ddf85df4069b7df4d0d6e0db4f13ac67166
Angelica Serebnitski <angelsa9292@gmail.com>
2022-04-24 19:05:49 +0300

    favorites page resposive

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -3,7 +3,7 @@ import { useHistory } from "react-router-dom";
 import { useDispatch, useSelector } from 'react-redux'
 import moment from 'moment'
 import { CSSTransition } from 'react-transition-group';
-import { Rings } from  'react-loader-spinner'
+import { Rings } from 'react-loader-spinner'
 
 import { loadNewCity } from '../store/soldays.action'
 
@@ -27,22 +27,22 @@ export const Favorites = () => {
             timeout={1000}
             classNames="fade"
         >
-            <div className="fav-locations-container" >
-                {favLoc.length ? favLoc.map((loc) => (
-                    <div className="weather-card" onClick={() => setCity(loc)}>
-                        <label>{loc.city[0].data[0].LocalizedName}</label>
-                        <div className="img-div">
-                            <img src={`https://www.accuweather.com/images/weathericons/${loc.city[0].currWeather[0].WeatherIcon}.svg`} />
+            <section className="weather-list">
+                    {favLoc.length ? favLoc.map((loc) => (
+                        <div className="weather-card" style={{color:"whitesmoke"}} onClick={() => setCity(loc)}>
+                            <label>{loc.city[0].data[0].LocalizedName}</label>
+                            <div className="img-div">
+                                <img src={`https://www.accuweather.com/images/weathericons/${loc.city[0].currWeather[0].WeatherIcon}.svg`} />
+                            </div>
+                            <label>{loc.city[0].currWeather[0].WeatherText}</label>
+                            <label>Date: {moment(loc.city[0].currWeather[0].LocalObservationDateTime).format("MMM D")}</label>
+                            <label>Temperature: {loc.city[0].currWeather[0].Temperature.Imperial.Value}{loc.city[0].currWeather[0].Temperature.Imperial.Unit}</label>
                         </div>
-                        <label>{loc.city[0].currWeather[0].WeatherText}</label>
-                        <label>Date: {moment(loc.city[0].currWeather[0].LocalObservationDateTime).format("MMM D")}</label>
-                        <label>Temperature: {loc.city[0].currWeather[0].Temperature.Imperial.Value}{loc.city[0].currWeather[0].Temperature.Imperial.Unit}</label>
-                    </div>
-                )):
-                <div className='loader'><Rings color="#fc8345" height={110} width={110} /> </div>
+                    )) :
+                        <div className='loader'><Rings color="#fc8345" height={110} width={110} /> </div>
 
-            }
-            </div>
+                    }
+            </section>
 
 
         </CSSTransition>
